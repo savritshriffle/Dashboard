1b7404968cc8baceb4127eb9d8ea26e877f1d37b
savritshriffle <savrit@shriffle.com>
2025-03-25 19:08:07 +0530

    Pagination data persist

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,7 +4,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { ContactComponent } from '../contact/contact.component';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort, Sort } from '@angular/material/sort';
-import { MatPaginator } from '@angular/material/paginator';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -17,42 +17,57 @@ export class HomeComponent implements OnInit, OnChanges{
 @ViewChild(MatSort) sort!: MatSort;
 @ViewChild(MatPaginator, {static: false}) paginator!: MatPaginator;
 posts: any[] = [];   
-pageIndex = 0;
+pageIndex = 1;
 filterData: any[] = []; 
 searchData: string = '';
-displayedColumns: string[] = ['userId', 'id', 'title', 'body', 'Action'];
+displayedColumns: string[] = ['userId', 'id', 'title', 'body', 'Action', 'Delete'];
 selectedPost:any = null;
 dataSources = new MatTableDataSource<any>([]);
 sortedData = new MatSort();
+currentPage = 0;
+currentSize = 0;
+constructor(private service: ServiceService, public dialog: MatDialog, private cdr: ChangeDetectorRef, private router : ActivatedRoute) {
 
- 
-constructor(private service: ServiceService, public dialog: MatDialog, private cdr: ChangeDetectorRef, private router : ActivatedRoute) {}
+}
 
 ngOnInit(): void { 
-  this.getPostData();
+    this.getPostData();
+  
+  
 }
 
 ngOnChanges(changes: SimpleChanges): void {
-  if (this.searchData.trim() === ''  ) 
+    if (this.searchData.trim() === ''  ) 
     {
-    return alert("Enter Valid Data"+" "+ changes);
+      return alert("Enter Valid Data"+" "+ changes);
     }
     this.filterData = this.posts.filter(user =>
-      user.userId.toString() === this.searchData || user.id.toString() === this.searchData
-    || user.title.startsWith('qui est esse'));
-    this.dataSources.data = this.filterData 
-}
+        user.userId.toString() === this.searchData || user.id.toString() === this.searchData
+    ||  user.title.startsWith('qui est esse'));
+        this.dataSources.data = this.filterData 
+    }
 
 
 getPostData() {
-  this.service.getPosts().subscribe((data) => {
+    this.service.getPosts().subscribe((data) => {
     this.posts = data; 
     data.forEach((element:any) => {
       element.isEdit = false;
     });
-   
     this.dataSources = new MatTableDataSource(data);
     this.dataSources.paginator = this.paginator
+  
+    localStorage.setItem('apiData', JSON.stringify(data));
+    
+    
+    const indexData  =  localStorage.getItem('paginatorIndex');
+    const sizeData = localStorage.getItem('paginatorSize')
+    
+    this.dataSources.paginator['pageIndex']= Number(indexData) 
+    this.dataSources.paginator['pageSize']= Number(sizeData)
+      
+    this.dataSources.paginator = this.paginator; 
+      
   });
 }
 
@@ -64,20 +79,40 @@ Edit(post: any) {
     post.isEdit= true
   }
   
-  save($event:any, post: any, key: any){
-    post[key] = $event.target.value;
-    this.dataSources.data.filter((value) => {
-      if(value.id === key) {
-        this.dataSources.data = post[key];
-        
-    }
-  })
+save(post: any){
+    post.isEdit = false;
+    this.dataSources.data = [...this.dataSources.data]
+}
+handleOnChange(e: any, post: any, key: any) {
+    post[key]= e.target.value;
+    this.dataSources.data = [...this.dataSources.data];
 }
 
-handleOnChnage(e: any, post: any, key: any) {
-  post[key]= e.target.value
+
+onPaginateChange(e: any) {
+    let index = this.dataSources.paginator?.pageIndex
+    let size  = this.dataSources.paginator?.pageSize
+
+    localStorage.setItem('paginatorIndex', JSON.stringify(index))
+    localStorage.setItem('paginatorSize', JSON.stringify(size))
 }
- 
+
+
+deleteData(id: string) {
+    // let index = this.dataSources.data.find((value) =>value.id === id) 
+    // this.dataSources.data.splice(index, 1)
+    // this.dataSources.data = [...this.dataSources.data]
+     console.log(id)
+     const dataApi = localStorage.getItem('apiData')
+     const move = localStorage.removeItem(id);
+     console.log(move)
+     console.log(dataApi?.includes(id))
+     
+     if(dataApi?.includes(id) == true) {
+       localStorage.removeItem(id)
+     }
+}     
+
 sortData(sort :Sort) {
     const data = this.posts.slice();
 
