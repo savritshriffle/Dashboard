d35adb3f5d397380ff73c632cbfbaae74425375b
savritshriffle <savrit@shriffle.com>
2025-04-19 23:16:31 -0700

    add new for conflict

---
+++ src/app/new-test/new-test.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-test',
+  templateUrl: './new-test.component.html',
+  styleUrls: ['./new-test.component.css']
+})
+export class NewTestComponent {
+   some() {
+    console.log('helloo')
+    console.log('helloo')
+    console.log('helloo')
+    console.log('helloo')
+    console.log('helloo')
+
+
+
+
+
+
+    console.log('hello')
+    console.log('hello')
+
+
+
+    console.log('hello');
+    console.log('hello')
+    
+   }
+}
