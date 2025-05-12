0ed83bf5362c94407d179d9903bf6d8e32889bac
savritshriffle <savrit@shriffle.com>
2025-05-12 15:53:45 +0530

    correct component name

---
+++ src/app/checkbox-filter/checkbox.component.ts
@@ -0,0 +1,114 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector:  'app-checkbox',
+  templateUrl:  './checkbox.component.html',
+  styleUrls:  ['./checkbox.component.css']
+})
+export class CheckboxComponent {
+  filterValue:  any[] = [];
+  filterData = [
+    {
+      name:  'complete',
+      value: false
+    },
+    {
+      name:  'Incomplete',
+      value: false
+    },
+  ]
+  studentData = [
+    {
+      id: 1,
+      name: 'Aadi',
+      contect: 10235,
+      complete: true,
+    },
+    {
+      id: 2,
+      name: 'Book',
+      contect: 10235,
+      complete: true
+    },
+    {
+      id: 3,
+      name: 'Cat',
+      contect: 10235,
+      complete: true
+    },
+    {
+      id: 4,
+      name: 'diya',
+      contect: 10235,
+      complete: false
+    },
+    {
+      id: 5,
+      name: 'Siva',
+      contect: 10235,
+      complete: false
+    },
+    {
+      id: 6,
+      name: 'Ram',
+      contect: 10235,
+      complete: true
+    },
+    {
+      id: 7,
+      name: 'Raghav',
+      contect: 10235,
+      complete: false
+    },
+    {
+      id: 8,
+      name: 'Siya',
+      contect: 10235,
+      complete: false
+    },
+    {
+      id: 9,
+      name: 'Riya',
+      contect: 10235,
+      complete: true
+    },
+    {
+      id: 10,
+      name: 'Priya',
+      contect: 10235,
+      complete: true
+    },
+    {
+      id: 10,
+      name: 'Priya',
+      contect: 10235,
+      complete: true
+    },
+    {
+      id: 11,
+      name: 'Tiya',
+      contect: 10235,
+      complete: true
+    },
+  ]
+  
+  onFilterData() {
+    const isCheckData = this.filterData.find((value) => value.name === 'complete')?.value;
+    const isInCheckedData = this.filterData.find((value) => value.name === 'Incomplete')?.value;
+    const trueValue = this.studentData.filter((val) => val.complete === true);
+    const falseValue = this.studentData.filter((val) => val.complete === false);
+    
+    if(isCheckData && isInCheckedData){
+     this.filterValue = [...this.studentData]
+    }
+    else if(isCheckData){
+      this.filterValue = [...trueValue]
+    }
+    else if(isInCheckedData){
+      this.filterValue = [...falseValue]
+    }
+    else {
+      this.filterValue = [...this.studentData]
+    }
+  }   
+}
---
+++ src/app/student-Data/student-Data.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StudentComponent } from './student-Data.component';
+
+describe('NewTestComponent', () => {
+  let component: StudentComponent;
+  let fixture: ComponentFixture<StudentComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [StudentComponent]
+    });
+    fixture = TestBed.createComponent(StudentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+ 
+  });
+});
