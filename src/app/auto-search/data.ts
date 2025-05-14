a4f3e771a7e0d7da4e47e1253e6327e1ed3ee9b8
savritshriffle <savrit@shriffle.com>
2025-05-14 14:49:45 +0530

    resolve comments change the name of service and components

--- src/app/auto-search/data.ts
+++ src/app/auto-search/data.ts
@@ -3,63 +3,63 @@ export const Data = [
       id:1,
       name: 'rahul',
       age: 20,
-      contact:1234567,
+      contact: 1234567,
       post: 'student'
     },
     {
       id:2,
       name: 'ram',
       age: 20,
-      contact:1234567,
+      contact: 1234567,
       post: 'student'
     },
     {
       id:3,
       name: 'kiran',
       age: 20,
-      contact:1234567,
+      contact: 1234567,
       post: 'student'
     },
     {
       id:4,
       name: 'jyoti',
       age: 20,
-      contact:1234567,
+      contact: 1234567,
       post: 'student'
     },
     {
       id:5,
       name: 'priya',
       age: 20,
-      contact:1234567,
+      contact: 1234567,
       post: 'student'
     },
     {
       id:7,
       name: 'savri',
       age: 20,
-      contact:1234567,
+      contact: 1234567,
       post: 'student'
     },
     {
       id:8,
       name: 'siya',
       age: 20,
-      contact:1234567,
+      contact: 1234567,
       post: 'student'
     },
     {
       id:9,
       name: 'riya',
       age: 20,
-      contact:1234567,
+      contact: 1234567,
       post: 'student'
     },
     {
       id:10,
       name: 'arya',
       age: 20,
-      contact:1234567,
+      contact: 1234567,
       post: 'student'
     },
   ]
